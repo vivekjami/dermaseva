4e3a3362f90c19bc1fc624b51ca8a678ba5f3cbf
vivekjami <j.vivekvamsi@gmail.com>
2026-04-22 10:55:43 +0000

    fix: remove invalid app.json fields, stub react-native-litert-lm for EAS build compatibility

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,13 @@
 const { getDefaultConfig } = require('expo/metro-config');
+const path = require('path');
 
 const config = getDefaultConfig(__dirname);
 
-config.resolver.assetExts = [...config.resolver.assetExts, 'txt'];
+config.resolver = {
+  ...config.resolver,
+  extraNodeModules: {
+    'react-native-litert-lm': path.resolve(__dirname, 'modules/stubs/react-native-litert-lm'),
+  },
+};
 
 module.exports = config;
