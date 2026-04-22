59875a49eaec66667b741cc79e1734c174c37409
vivekjami <j.vivekvamsi@gmail.com>
2026-04-22 11:01:54 +0000

    fix: add RAG source documents, register .txt as Metro asset extension

--- metro.config.js
+++ metro.config.js
@@ -3,8 +3,10 @@ const path = require('path');
 
 const config = getDefaultConfig(__dirname);
 
+// Allow Metro to bundle .txt files as static assets for RAG documents
 config.resolver = {
   ...config.resolver,
+  assetExts: [...config.resolver.assetExts, 'txt'],
   extraNodeModules: {
     'react-native-litert-lm': path.resolve(__dirname, 'modules/stubs/react-native-litert-lm'),
   },
